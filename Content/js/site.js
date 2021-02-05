73f6dbbe2ac51d310da47f4264f7cd5c60bf6ccb
Mack <macknz7@gmail.com>
2021-02-05 14:18:38 -0800

    Refactor makes functions more abstract

--- Content/js/site.js
+++ Content/js/site.js
@@ -1,50 +1,46 @@
-﻿const PROVIDING_AGENCY_CITY = document.getElementById('ProvidingAgency_City');
+﻿// city
+const PROVIDING_AGENCY_CITY = document.getElementById('ProvidingAgency_City');
 const PROVIDING_AGENCY_CITY_DROPDOWN = document.getElementById('providingAgencyCityDropdown');
+// state
 const PROVIDING_AGENCY_STATE = document.getElementById('ProvidingAgency_State');
 const PROVIDING_AGENCY_STATE_DROPDOWN = document.getElementById('providingAgencyStateDropdown');
+// county
 const PROVIDING_AGENCY_COUNTY = document.getElementById('ProvidingAgency_County');
 const PROVIDING_AGENCY_COUNTY_DROPDOWN = document.getElementById('providingAgencyCountyDropdown');
 
-const JAIL_CITY = document.getElementById('Jail_City');
-const JAIL_STATE = document.getElementById('Jail_State');
-const JAIL_COUNTY = document.getElementById('Jail_County');
+// when user has input the city
+PROVIDING_AGENCY_CITY.addEventListener('blur', async function () {
+    // Capitalize input
+    let city = PROVIDING_AGENCY_CITY.value[0].toUpperCase() + PROVIDING_AGENCY_CITY.value.slice(1).toLowerCase();
+    PROVIDING_AGENCY_CITY.value = city;
 
-const REQUESTING_AGENCY_CITY = document.getElementById('RequestingAgency_City');
-const REQUESTING_AGENCY_STATE = document.getElementById('RequestingAgency_State');
-const REQUESTING_AGENCY_COUNTY = document.getElementById('RequestingAgency_County');
+    let states = [];
+    let options = await getData("city", city);
+    options.forEach(option => states.push(option.state));
 
-async function getStates(search) {
-    // search "db"
+    // put options in dropdown
+    populateDropdown(states, PROVIDING_AGENCY_STATE, PROVIDING_AGENCY_STATE_DROPDOWN);
+});
+
+// returns data that have a given type; either: city or state
+async function getData(type, search) {
+    // search "db" ~ 6000 cities in various states
     return await fetch('https://gist.githubusercontent.com/mackenzieweaver/c848adf78ebac73a38ffa38f1d65370e/raw/70544e6dac9fbf435d0fae68e44fdd6f0c26b940/gistfile1.txt')
         .then(res => res.json())
         .then(d => {
-            return d.filter(objs => objs.city == search);
+            return d.filter(objs => objs[type] == search);
         });
 }
 
-PROVIDING_AGENCY_CITY.addEventListener('blur', async function () {
-    // Capitalize input
-    let search = PROVIDING_AGENCY_CITY.value[0].toUpperCase() + PROVIDING_AGENCY_CITY.value.slice(1).toLowerCase();
-    PROVIDING_AGENCY_CITY.value = search;
-
-    let states = [];
-    let options = await getStates(search);
-    options.forEach(option => states.push(option.state));
-    populateProvidingAgencyStateOptions(states);
-});
-
-
-
-function populateProvidingAgencyStateOptions(states) {
-    PROVIDING_AGENCY_STATE_DROPDOWN.innerHTML = '';
+function populateDropdown(data, box, dropdown) {
+    dropdown.innerHTML = '';
     let html = '';
-    states.forEach(s => {
-        html += `<button class="a dropdown-item" onclick="populateState(event, '${s}')">${s}</button>`;
+    data.forEach(val => {
+        html += `<button type="button" class="a dropdown-item" onclick="populateBox(${box}, '${val}')">${val}</button>`;
     });
-    PROVIDING_AGENCY_STATE_DROPDOWN.innerHTML = html;
+    dropdown.innerHTML = html;
 }
 
-function populateState(event, state) {
-    event.preventDefault();
-    PROVIDING_AGENCY_STATE.value = state;
+function populateBox(box, val){
+    box.value = val;
 }
