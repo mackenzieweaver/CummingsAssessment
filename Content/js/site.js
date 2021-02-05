e94caf5465b38528af9ca470cf291931f2028010
Mack <macknz7@gmail.com>
2021-02-05 11:03:12 -0800

    Providing Agency Dropdowns

---
+++ Content/js/site.js
@@ -0,0 +1,11 @@
+﻿const PROVIDING_AGENCY_CITY = document.getElementById('ProvidingAgency_City');
+const PROVIDING_AGENCY_STATE = document.getElementById('ProvidingAgency_State');
+const PROVIDING_AGENCY_COUNTY = document.getElementById('ProvidingAgency_County');
+
+const JAIL_CITY = document.getElementById('Jail_City');
+const JAIL_STATE = document.getElementById('Jail_State');
+const JAIL_COUNTY = document.getElementById('Jail_County');
+
+const REQUESTING_AGENCY_CITY = document.getElementById('RequestingAgency_City');
+const REQUESTING_AGENCY_STATE = document.getElementById('RequestingAgency_State');
+const REQUESTING_AGENCY_COUNTY = document.getElementById('RequestingAgency_County');
