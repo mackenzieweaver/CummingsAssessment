289856f75627a4c87d8c9bb893dfbe6512048d8e
Mack <macknz7@gmail.com>
2021-02-05 11:22:32 -0800

    Data saves to mssqllocaldb

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,6 +10,8 @@ namespace CummingsAssessment.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
             return View();
@@ -22,9 +25,28 @@ namespace CummingsAssessment.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Agreement(AgreementViewModel model)
+        public async Task<ActionResult> Agreement(AgreementViewModel model)
         {
-            return View();
+            db.ProvidingAgencies.Add(model.ProvidingAgency);
+            db.Jails.Add(model.Jail);
+            db.BondTransfers.Add(model.BondTransfer);
+            db.RequestingAgencies.Add(model.RequestingAgency);
+            db.Defendants.Add(model.Defendant);
+            db.Indemnitors.Add(model.Indemnitor);
+            await db.SaveChangesAsync();
+
+            db.Agreements.Add(new Agreement
+            {
+                ProvidingAgencyId = model.ProvidingAgency.Id,
+                JailId = model.Jail.Id,
+                BondTransferId = model.BondTransfer.Id,
+                RequestingAgencyId = model.RequestingAgency.Id,
+                DefendantId = model.Defendant.Id,
+                IndemnitorId = model.Indemnitor.Id,
+                AdditionalInformation = model.AdditionalInformation
+            });
+            await db.SaveChangesAsync();
+            return RedirectToAction(nameof(Agreement));
         }
 
         public ActionResult About()
